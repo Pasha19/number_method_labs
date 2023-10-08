fd4d4364a4d764a1f794e282ba26ff78ac1f9b34
Pasha19 <20466125+Pasha19@users.noreply.github.com>
2023-10-08 18:35:19 +0300

    lab3 gauss_max_element

--- lab3/__init__.py
+++ lab3/__init__.py
@@ -29,16 +29,35 @@ def method_gauss(matrix: npt.NDArray) -> npt.NDArray:
     return reverse(matrix)
 
 
+def helper_rect(matrix: npt.NDArray, k: int) -> None:
+    shape = matrix.shape
+    for i in range(k + 1, shape[0]):
+        for j in range(k + 1, shape[1]):
+            matrix[i][j] -= matrix[k][j] * matrix[i][k] / matrix[k][k]
+    matrix[k] /= matrix[k][k]
+    matrix[k + 1:, k] = 0.0
+
+
 def method_exclude_rect(matrix: npt.NDArray) -> npt.NDArray:
     shape = matrix.shape
     if len(shape) != 2 or shape[0] + 1 != shape[1]:
         raise MatrixSizeException()
 
     for k in range(shape[0]):
-        for i in range(k + 1, shape[0]):
-            for j in range(k + 1, shape[1]):
-                matrix[i][j] -= matrix[k][j] * matrix[i][k] / matrix[k][k]
-        matrix[k] /= matrix[k][k]
-        matrix[k + 1:, k] = 0.0
+        helper_rect(matrix, k)
+
+    return reverse(matrix)
+
+
+def method_gauss_max_element(matrix: npt.NDArray) -> npt.NDArray:
+    shape = matrix.shape
+    if len(shape) != 2 or shape[0] + 1 != shape[1]:
+        raise MatrixSizeException()
+
+    for k in range(shape[0]):
+        max_ind = abs(matrix[k:, k]).argmax() + k
+        if k != max_ind:
+            matrix[[k, max_ind]] = matrix[[max_ind, k]]
+        helper_rect(matrix, k)
 
     return reverse(matrix)
--- lab3/test_lab3.py
+++ lab3/test_lab3.py
@@ -20,3 +20,16 @@ def test_methods(method):
     matrix = slae[:shape[0], :shape[0]]
     col = slae[:, [shape[0]]]
     assert np.allclose(matrix.dot(solution), col)
+
+
+def test_method_gauss_max_element():
+    slae = np.array([
+        [-3,  2.099, 6, 3.901],
+        [10, -7,     0, 7],
+        [ 5, -1,     5, 6],
+    ], dtype="float64")
+    solution = lab3.method_gauss_max_element(np.copy(slae))
+    shape = slae.shape
+    matrix = slae[:shape[0], :shape[0]]
+    col = slae[:, [shape[0]]]
+    assert np.allclose(matrix.dot(solution), col)
