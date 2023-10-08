8e6117a27aa10786412e56d6c4e0e11f0f68c1c6
Pasha19 <20466125+Pasha19@users.noreply.github.com>
2023-10-08 17:52:26 +0300

    lab3 exclude_rect

--- lab3/__init__.py
+++ lab3/__init__.py
@@ -6,20 +6,39 @@ class MatrixSizeException(Exception):
     pass
 
 
-def method_gauss(matr: npt.NDArray) -> npt.NDArray:
+def reverse(matr: npt.NDArray) -> npt.NDArray:
     shape = matr.shape
-    if len(shape) != 2 or shape[0] + 1 != shape[1]:
-        raise MatrixSizeException()
-
-    for i in range(shape[0]):
-        matr[i] /= matr[i][i]
-        for j in range(i + 1, shape[0]):
-            matr[j] -= matr[j][i] * matr[i]
-
     result = np.zeros((shape[0], 1))
     for i in range(shape[0] - 1, -1, -1):
         result[i] = matr[i][shape[0]]
         for j in range(shape[0] - 1, i, -1):
             result[i] -= result[j] * matr[i][j]
-
     return result
+
+
+def method_gauss(matrix: npt.NDArray) -> npt.NDArray:
+    shape = matrix.shape
+    if len(shape) != 2 or shape[0] + 1 != shape[1]:
+        raise MatrixSizeException()
+
+    for i in range(shape[0]):
+        matrix[i] /= matrix[i][i]
+        for j in range(i + 1, shape[0]):
+            matrix[j] -= matrix[j][i] * matrix[i]
+
+    return reverse(matrix)
+
+
+def method_exclude_rect(matrix: npt.NDArray) -> npt.NDArray:
+    shape = matrix.shape
+    if len(shape) != 2 or shape[0] + 1 != shape[1]:
+        raise MatrixSizeException()
+
+    for k in range(shape[0]):
+        for i in range(k + 1, shape[0]):
+            for j in range(k + 1, shape[1]):
+                matrix[i][j] -= matrix[k][j] * matrix[i][k] / matrix[k][k]
+        matrix[k] /= matrix[k][k]
+        matrix[k + 1:, k] = 0.0
+
+    return reverse(matrix)
--- lab3/test_lab3.py
+++ lab3/test_lab3.py
@@ -1,15 +1,22 @@
+import pytest
 import lab3
 import numpy as np
 
 
-def test_method_gauss():
+def get_methods():
+    yield lab3.method_gauss
+    yield lab3.method_exclude_rect
+
+
+@pytest.mark.parametrize("method", get_methods())
+def test_methods(method):
     slae = np.array([
         [2, 1, 4, 16],
         [3, 2, 1, 10],
         [1, 3, 3, 16],
     ], dtype="float64")
-    solution = lab3.method_gauss(np.copy(slae))
+    solution = method(np.copy(slae))
     shape = slae.shape
-    matr = slae[:shape[0], :shape[0]]
+    matrix = slae[:shape[0], :shape[0]]
     col = slae[:, [shape[0]]]
-    assert np.allclose(matr.dot(solution), col)
+    assert np.allclose(matrix.dot(solution), col)
